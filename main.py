183036560f329e1b0bf9998916097ed6b10926d0
Naveen Rajarapu <naveenrajarapuiiit@gmail.com>
2025-06-11 22:28:37 -0400

    Fixed port binding for Render deployment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///checklog.db'
@@ -55,4 +56,6 @@ def history():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
